b07475c26111ba27b09ef235baa9827fed843f70
Gregory Rice <gregrice@gmail.com>
2016-06-17 13:20:42 -0700

    Add rich location

---
+++ src/main/scala/com/sanoma/cda/geoip/RichIpLocation.scala
@@ -0,0 +1,74 @@
+package com.sanoma.cda.geoip
+
+import com.sanoma.cda.geo._
+import com.sanoma.cda.geo.model._
+import collection.JavaConverters._
+
+/**
+ * Simplified IpLocation information
+ */
+
+abstract class LocationLike
+
+case class IpLocation(
+  countryCode: Option[String],
+  countryName: Option[String],
+  region:      Option[String], // should look more into this...
+  city:        Option[String],
+  geoPoint:    Option[Point],
+  postalCode:  Option[String],
+  continent:   Option[String]
+) extends LocationLike
+
+object IpLocation {
+
+  def apply(richIpLocation: RichIpLocation): IpLocation = new IpLocation(
+    richIpLocation.country.map(_.isoCode),
+    richIpLocation.country.map(_.name),
+    richIpLocation.region.map(_.name),
+    richIpLocation.city.map(_.name),
+    richIpLocation.geoPoint,
+    richIpLocation.postalCode.map(_.code),
+    richIpLocation.continent.map(_.name)
+  )
+
+}
+
+/**
+ * Case class to hold the location information from MaxMind.
+ */
+case class RichIpLocation(
+  country:      Option[Country],
+  subdivisions: Option[List[Subdivision]], // should look more into this...
+  city:         Option[City],
+  geoPoint:     Option[Point],
+  postalCode:   Option[Postal],
+  continent:    Option[Continent],
+  location:     Option[Location]
+) extends LocationLike {
+
+  def region: Option[Subdivision] = subdivisions.flatMap(_.lastOption)
+
+}
+
+// MaxMind
+import com.maxmind.geoip2.model.CityResponse
+
+/**
+ * Companion object to help convert MaxMind OmniResponse to IpLocation
+ */
+object RichIpLocation {
+
+  /**
+   * Constructs a RichIpLocation from a MaxMind Location
+   */
+  def apply(omni: CityResponse): RichIpLocation = new RichIpLocation(
+    if (omni.getCountry.getGeoNameId != null) Option(omni.getCountry).map(Country(_)) else None,
+    if (omni.getSubdivisions.size != 0) Option(omni.getSubdivisions.asScala.toList.map(Subdivision(_))) else None,
+    if (omni.getCity.getGeoNameId != null) Option(omni.getCity).map(City(_)) else None,
+    if (omni.getLocation != null) Point.combineLatLong(Point.jDoubleOptionify(omni.getLocation.getLatitude), Point.jDoubleOptionify(omni.getLocation.getLongitude)) else None,
+    if (omni.getPostal.getCode != null) Option(omni.getPostal).map(Postal(_)) else None,
+    if (omni.getContinent.getCode != null) Option(omni.getContinent).map(Continent(_)) else None,
+    if (omni.getLocation != null) Option(omni.getLocation).map(Location(_)) else None
+  )
+}
