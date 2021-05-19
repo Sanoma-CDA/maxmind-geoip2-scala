4706eda3ef41cea6119fdec05374e852a68c316c
Rotem Fogel <rotem@seekingalpha.com>
2021-05-19 17:02:03 +0300

    deploy

--- src/test/resources/download_GeoLite2-City.sh
+++ src/test/resources/download_GeoLite2-City.sh
@@ -1,3 +1,3 @@
-echo Downloading GeoLite2-City.mmdb.gz
-aws s3 cp s3://seekingalpha-data/accessories/maxmind/GeoLite2-City.mmdb .
+echo Downloading GeoLite2-City.mmdb
+aws s3 cp s3://seekingalpha-data/accessories/maxmind/GeoLite2-City.mmdb ./src/test/resources/GeoLite2-City.mmdb
 echo Note: some of the tests might fail if the contents of the DB has changed
